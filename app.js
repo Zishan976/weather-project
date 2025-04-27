9d4474eec36f8d2d6be9de257a1aeacb39c919fe
Zishan <sharkarzishan@gmail.com>
2025-04-27 23:08:23 +0600

    update code

--- app.js
+++ app.js
@@ -1,57 +1,54 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
-require('dotenv').config();
-console.log(`Weather_api_key: ${process.env.Weather_api_key}`);
+require("dotenv").config();
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Set EJS as the templating engine
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
-
-    res.sendFile(__dirname + "/index.html");
-
-
+  res.sendFile(__dirname + "/views/index.html");
 });
 
 app.post("/", (req, res) => {
+  const city = req.body.city;
+  const apiKey = process.env.Weather_api_key;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
 
-    const query = req.body.city;
-    const capQuery = query.slice(0, 1).toUpperCase() + query.slice(1, query.length).toLowerCase();
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&units=metric&appid=${process.env.Weather_api_key}&q=` + capQuery;
-
-    https.get(url, (response) => {
-        if (response.statusCode === 200) {
-
-            response.on("data", (data) => {
-                const weatherdata = JSON.parse(data);
-                const location = weatherdata.name;
-                const temp = weatherdata.main.temp;
-                const condition = weatherdata.weather[0].description;
-                const icon = weatherdata.weather[0].icon;
-                const img = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
-                res.write("<h1>The temperature in " + location + " is " + temp + " deg C.</h1>");
-                res.write("<h3>Current weather is " + condition + ".</h3>");
-                res.write("<img src=" + img + " alt='weather img'>")
-
-                res.send();
-            })
+  https
+    .get(url, (apiResponse) => {
+      let data = "";
+      apiResponse.on("data", (chunk) => {
+        data += chunk;
+      });
+      apiResponse.on("end", () => {
+        const weatherData = JSON.parse(data);
+        if (apiResponse.statusCode === 200) {
+          res.render("result", {
+            location: weatherData.name,
+            temperature: weatherData.main.temp,
+            condition: weatherData.weather[0].description,
+            icon: weatherData.weather[0].icon,
+          });
         } else {
-
-            res.status(401).send("Did not find any weather data");
-        };
+          res.send("<h1>Error: City not found. Please try again.</h1>");
+        }
+      });
     })
+    .on("error", (err) => {
+      res
+        .status(500)
+        .send(
+          "<h1>Error: Could not fetch weather data. Please try again later.</h1>"
+        );
+    });
+});
 
-
-})
-
-
-
-
-
-const port = process.env.port || 3000
-
-app.listen(3000, () => {
-    console.log(`sarver is running on port ${port}.`);
-
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
